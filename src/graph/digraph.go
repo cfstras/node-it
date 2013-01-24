a2145c37dcdf521c4cefe22896b5e50cdfc5bf56
cfstras <claus@strasburger.de>
2013-01-24 22:26:33 +0100

    changes to digraph

--- src/graph/digraph.go
+++ src/graph/digraph.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"fmt"
 	"os/exec"
+	"time"
 )
 
 func MakeGraph() {
@@ -21,11 +22,12 @@ func makeFile() bool {
 	}
 	defer file.Close()
 	
-	file.WriteString("digraph {\n")
-	file.WriteString(`graph [splines=spline, overlap=false, fontname="Calibri", dpi=120, size=20, ratio=1.6]`)
-	file.WriteString("\n")
+	file.WriteString("digraph G {\n")
+	file.WriteString("graph [splines=spline, overlap=false, fontname=\"Myriad Pro\", dpi=120, size=30, ")
+	file.WriteString("ratio=1.6, orientation=landscape]\n")
+	file.WriteString("node [fillcolor=\"#eeeeee\", color=\"#aaaaaa\", style=filled, shape=box]\n")
 	for _, r := range Links {
-		file.WriteString(r.From+" -> "+r.To+"\n")
+		file.WriteString("\""+r.From+"\" -> \""+r.To+"\";\n")
 	}
 	file.WriteString("}\n")
 	return true
@@ -33,11 +35,13 @@ func makeFile() bool {
 
 func runGV() {
 	fmt.Println("Running sfdp")
+	start := time.Now()
 	cmd := exec.Command("sfdp","-o","graph.png","-Tpng","graph.dot")
 	st, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println("Error while running sfdp: ", err)
 	}
 	fmt.Println(string(st))
+	fmt.Println("time: ", time.Since(start))
 	fmt.Println("sfdp finished.")
 }
